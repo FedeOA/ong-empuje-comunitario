3d5ac72658d637d539486299b7db9b495174a6bb
FedeOA <fedeacosta07@hotmail.com>
2025-09-23 22:07:23 -0300

    integracion de eventos

--- cliente/src/main/java/com/grpc/demo/mapper/event/EventMapperImpl.java
+++ cliente/src/main/java/com/grpc/demo/mapper/event/EventMapperImpl.java
@@ -5,16 +5,26 @@ import com.grpc.demo.mapper.IMapper;
 import com.grpc.demo.service.event.Event;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class EventMapperImpl implements IMapper <Event, EventResponseDTO>{
     @Override
     public EventResponseDTO map(Event source) {
 
+        List<String> users = new ArrayList<>();
+
+        for(int i = 0;i<source.getUsersList().size();i++){
+            users.add(source.getUsers(i));
+        }
+
         return new EventResponseDTO(
                 source.getId(),
                 source.getName(),
                 source.getDescription(),
-                source.getFechaHora()
+                source.getFechaHora(),
+                users
         );
     }
 }
