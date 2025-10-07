632b602d9538149ebb035522518eb74078cbcbd2
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-10-06 21:49:01 -0300

    Corrección Servers main.py

--- servers/services_pb2_grpc/event_pb2_grpc.py
+++ servers/services_pb2_grpc/event_pb2_grpc.py
@@ -3,9 +3,9 @@
 import grpc
 import warnings
 
-import services_pb2.event_pb2 as event__pb2
+from services_pb2 import event_pb2 as event__pb2
 
-GRPC_GENERATED_VERSION = '1.74.0'
+GRPC_GENERATED_VERSION = '1.75.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
