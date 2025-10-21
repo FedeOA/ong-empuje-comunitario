229f4b0401256c1bef76de87ac3cd98cc02cd37d
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-10-21 18:54:56 -0300

    Hot Fix: Donation Reports

--- frontend/src/pages/DonationReports.jsx
+++ frontend/src/pages/DonationReports.jsx
@@ -46,7 +46,6 @@ export default function DonationReport() {
               description
               quantity
               createdAt
-              createdByUsername
               deleted
             }
           }
