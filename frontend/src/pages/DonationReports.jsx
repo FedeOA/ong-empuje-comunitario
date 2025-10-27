c0295bca6e7f1037aea2147319200716ae90276e
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-10-27 17:27:04 -0300

    Correccion Donation Report Filters

--- frontend/src/pages/DonationReports.jsx
+++ frontend/src/pages/DonationReports.jsx
@@ -39,7 +39,6 @@ export default function DonationReport() {
               description
               quantity
               createdAt
-              createdByUsername
               deleted
             }
           }
