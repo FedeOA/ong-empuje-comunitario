f180a5eeee380f3236a6f02aa997c3c46b80d0cf
FedeOA <fedeacosta07@hotmail.com>
2025-10-28 19:59:52 -0300

    events

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -253,6 +253,12 @@ return (
         >
           Filtros
         </button>   
+        <button
+            className="bg-gray-600 text-white px-4 py-2 rounded-lg hover:bg-gray-700 transition"
+            onClick={fetchEvents}
+          >
+            Resetear Filtro
+          </button>
       </div>
     </div>
 
